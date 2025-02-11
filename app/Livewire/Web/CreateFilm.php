15196d296aa31c6c502268d0abd66822e1751c9a
Giovanna Andreoli <gibsandreoli.GA@gmail.com>
2025-02-11 00:27:02 -0300

    Update routes

--- app/Livewire/Web/CreateFilm.php
+++ app/Livewire/Web/CreateFilm.php
@@ -6,6 +6,9 @@ use Livewire\Component;
 
 class CreateFilm extends Component
 {
+    
+   
+
     public function render()
     {
         return view('livewire.web.create-film');
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', \App\Livewire\Web\Home::class)->name('home');
 Route::get('/cadastrar', \App\Livewire\Web\CreateFilm::class)->name('create');
 Route::get('/ver', \App\Livewire\Web\ViewFilm::class)->name('see');
+Route::post('/salvar-filme', function (Request $request) {
+    $request->validate([
+        'tittle' => 'required',
+        'summary' => 'required',
+        'cover ' => 'required|integer|min:1895|max:' . date('Y'),
+    ]);
+
+    Filme::create([
+        'tittle' => $request->input('tittle'),
+        'summary' => $request->input('summary'),
+        'cover' => $request->input('cover'),
+    ]);
+
+    return redirect()->back()->with('success');
+});
